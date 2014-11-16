f6e963150bcd4f6da5ad971cd9a60a9aba9bbb63
hackie <mr.hackie.chain@gmail.com>
2014-11-16 17:00:03 +0800

    changes

--- training.py
+++ training.py
@@ -1,11 +1,54 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import MySQLdb
+import sys
+from db import conn_db
 
-def main():
-	
-	return 0
+cur = conn_db().cursor() 
+
+def get_word(word):
+    word = word.strip()
+    word_list = []
+    cur.execute('SELECT * FROM word WHERE word = "%s"' %(word))
+    rows = cur.fetchall()
+    for row in rows:
+        word_map= {}
+        word_map["word"]  = row[0]
+        word_map["hash"]  = row[1]
+        word_map["ref_word"]  = row[2]
+        word_map["part_of_speech"]  = row[3]
+        word_map["definition"]  = row[4]
+        word_map["examples"]  = row[5]
+        word_list.append(word_map)
+    return word_list
+
+def reading_train(word_list):
+    for i in word_list:
+        print i["examples"]
+        while True:
+            rt = raw_input("Learned? (y/n)").strip()
+            if rt in ["y","n"]:
+                break            
+
+def spelling_train(word_list):
+    pass
+
+def main(argv):
+    f = open(argv[0])
+    for i in f.readlines():
+        word_list = get_word(i)
+        if len(word_list) == 0:
+            print "Miss %s" % (i)
+            continue
+        if argv[1] == "read":
+            reading_train(word_list)
+        elif argv[1] == "spell":
+            spelling_train(word_list)
+        else:
+            pass
+            
+    return 0
 
 if __name__ == '__main__':
-	main()
+	main(sys.argv[1:])
 
--- word_grab.py
+++ word_grab.py
@@ -5,6 +5,8 @@ import MySQLdb
 from pprint import pprint
 from bs4 import BeautifulSoup
 from task_runner import TaskRunner, Task
+from db import conn_db
+
 def get_uri(word):
     base_uri = 'http://www.collinsdictionary.com/dictionary/american-cobuild-learners/%s'
     return (base_uri % word)
@@ -16,6 +18,7 @@ def get_word_content(word):
         response = urllib2.urlopen(request)
         page = response.read()
     except Exception as e:
+        print e
         page = ""
     return page
 
@@ -29,9 +32,17 @@ def request_word(word):
     word = word.strip()
     if len(word) ==0:
         return None, False
-    page = get_word_content(word)
-    if not page:
-        return None, False
+    retry = 3
+    while True:
+        page = get_word_content(word)
+        if not page:
+            if retry !=0:
+                retry = retry - 1
+                continue
+            else:
+                return None, False
+        else:
+            break
     pool = BeautifulSoup(page)
     entry = []
     word_found = False
@@ -58,19 +69,15 @@ def request_word(word):
             entry.append((title,explanations))
     return entry, word_found
 
-def conn_db():
-    return MySQLdb.connect(host="localhost", user="root", passwd="", db="wordbank", charset='utf8')
-
-
 def write_word_entries(query_word, entry, word_found):
     db = conn_db()
     cur = db.cursor() 
     word = query_word
     if not word_found:
        ref_word = entry[0][0]
-       hash = hashlib.md5(query_word).hexdigest()
+       hashchar = hashlib.md5(query_word).hexdigest()
        try:
-           cur.execute("INSERT INTO word (hash, word, ref_word) VALUES (%s,%s,%s)", (hash, word, ref_word) )
+           cur.execute("INSERT INTO word (hash, word, ref_word) VALUES (%s,%s,%s)", (hashchar, word, ref_word) )
        except MySQLdb.IntegrityError, e:
            print "Dup %s" % (title)
        except MySQLdb.Error, e: 
@@ -81,12 +88,12 @@ def write_word_entries(query_word, entry, word_found):
         title, explanations = i
         for j in explanations:
             part_of_speech, definition, exmaples = j
-            hash = hashlib.md5(title.encode('ascii', 'ignore') + definition.encode('ascii', 'ignore')).hexdigest()
+            hashchar = hashlib.md5(title.encode('ascii', 'ignore') + definition.encode('ascii', 'ignore')).hexdigest()
             try:
                 definition = MySQLdb.escape_string(definition.encode("utf8"))
-                exmaples = MySQLdb.escape_string((u"\t".join(exmaples)).encode("utf8"))
-                query = u'INSERT INTO word (hash, word, ref_word, part_of_speech, definition, examples) VALUES ("%s","%s","%s","%s","%s","%s")'  \
-							%  (hash, title, None, part_of_speech, definition.decode("utf8"), exmaples.decode("utf8"))
+                exmaples = MySQLdb.escape_string((u"\n".join(exmaples)).encode("utf8"))
+                query = u'INSERT INTO word (hash, word, part_of_speech, definition, examples) VALUES ("%s","%s","%s","%s","%s")'  \
+							%  (hashchar, title, part_of_speech, definition.decode("utf8"), exmaples.decode("utf8"))
                 cur.execute(query)
             except MySQLdb.IntegrityError, e:
                 print "Dup %s" % (title)
@@ -108,17 +115,16 @@ class WordGrabTask(Task):
             print "No entry %s" %(obj)
             return False
         write_word_entries(obj, entry, word_found)
-        print "Done",obj
+        print "Done %s" %(obj)
         return True
  
 def main(argv):
-    file = open(argv[0])
+    f = open(argv[0])
     
-    task_runner = TaskRunner(5)
+    task_runner = TaskRunner(10)
     task_runner.start()
-    for i in file.readlines():
-		task_runner.add_task(WordGrabTask(i))
-    
+    for i in f.readlines():
+        task_runner.add_task(WordGrabTask(i))
     task_runner.join()
 	
 	
